02c21e487117b1124750d8d96b21d88f4f624e66
vlee20 <vlee20@protonmail.com>
2024-07-01 16:18:26 -0700

    framer motion package

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,3 +1,83 @@
+
+// motion and useAnimation Hooks from Framer Motion;
+// useEffect Hook from React;
+// useInView Hook from 'react-intersection-observer'
+import { motion, useAnimation } from "framer-motion";
+
+import { useInView } from "react-intersection-observer";
+
+import { useEffect } from "react";
+
+import "../styles/Contact.css";
+
+const boxVariant = {
+  visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
+  hidden: { opacity: 0, scale: 0 }
+};
+
+
+const Box = ({ num }) => {
+
+  const control = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      control.start("visible");
+    } else {
+      control.start("hidden");
+    }
+  }, [control, inView]);
+
+  return (
+    <motion.div
+      className="box"
+      ref={ref}
+      variants={boxVariant}
+      initial="hidden"
+      animate={control}
+    >
+      <h1 className="pgbreak">Box {num} </h1>
+    </motion.div>
+  );
+};
+
+
+
 export default function Contact() {
-  return <h1>Contact</h1>;
+  const control = useAnimation()
+  const [ref, inView] = useInView()
+  useEffect(() => {
+    if (inView) {
+      control.start("visible");
+    } else {
+      control.start("hidden");
+    }
+  }, [control, inView]);
+
+  return (
+    <>
+      {/* <div>
+        <motion.div variants={boxVariant}
+          ref={ref}
+          initial="hidden"
+          whileInView="visible"
+          animate={control}>
+          <h1 className="pgbreak">Contact</h1>
+        </motion.div>
+        <motion.div variants={boxVariant}
+          ref={ref}
+          initial="hidden"
+          whileInView="visible"
+          animate={control}>
+          <h1 className="pgbreak">Contact2</h1>
+        </motion.div>
+      </div> */}
+    <div className="App">
+      <Box num={1} />
+      <Box num={2} />
+      <Box num={3} />
+    </div>
+    </>
+  );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,17 @@ import fatty_tuna_jp from "../img/R0000918.JPG";
 import under_freeway_jp from "../img/R0000930.JPG";
 import sakura_jp from "../img/R0000961.JPG";
 import hakone_shrine_jp from "../img/R0001306.JPG";
+import "../styles/Home.css";
+
+// motion and useAnimation Hooks from Framer Motion;
+// useEffect Hook from React;
+// useInView Hook from 'react-intersection-observer'
+import { motion, useAnimation } from "framer-motion";
+
+import { useInView } from "react-intersection-observer";
+
+import { useEffect } from "react";
+
 
 export default function Home() {
   return (
@@ -17,35 +28,39 @@ export default function Home() {
             <h3>I like to travel if you can't tell!</h3>
           </Animator>
         </ScrollPage>
-        <ScrollPage page={1}>
-          <Animator animation={batch(Sticky(25), Fade())}>
-            <img
-              src={mammoth_mountain}
-              alt={"Taken leaving Mammoth Mountain"}
-              style={{
-                width: 700,
-                height: 700,
-              }}
-            />
-          </Animator>
-          <Animator animation={batch(Sticky(75), Fade())}>
-            <p>
-              This is located near the Mammoth Mountain resort.
-              Mammoth has always been an annual trip with friends. We always make memories riding down the slopes.
-              Snowboarding has been one of my favorite sports because of how scenic and fun it is.
-            </p>
-          </Animator>
-        </ScrollPage>
+        <div className='container'>
+          <ScrollPage page={1}>
+            <Animator animation={batch(Sticky(25), Fade())}>
+              <img
+                src={mammoth_mountain}
+                alt={"Taken leaving Mammoth Mountain"}
+                style={{
+                  maxWidth: 700,
+                  maxHeight: 700,
+                }}
+              />
+            </Animator>
+            <Animator animation={batch(Sticky(75), Fade())}>
+              <p>
+                This is located near the Mammoth Mountain resort.
+                Mammoth has always been an annual trip with friends. We always make memories riding down the slopes.
+                Snowboarding has been one of my favorite sports because of how scenic and fun it is.
+              </p>
+            </Animator>
+          </ScrollPage>
+        </div>
         <ScrollPage page={2}>
           <Animator animation={batch(Sticky(25), Fade())}>
-          <img
-              src={lake_tahoe_lake}
-              alt={"Taken leaving Lake Tahoe"}
-              style={{
-                width: 700,
-                height: 700,
-              }}
-            />
+            <div className='single_container'>
+              <img
+                src={lake_tahoe_lake}
+                alt={"Taken leaving Lake Tahoe"}
+                style={{
+                  maxWidth: 700,
+                  maxHeight: 700,
+                }}
+              />
+            </div>
           </Animator>
           <Animator animation={batch(Sticky(75), Fade())}>
             <p>
@@ -53,51 +68,36 @@ export default function Home() {
               It was a crazy drive from SoCal all the way to San Jose. Then from San Jose to Lake Tahoe.
               The amount of time driving was insane because it was during a storm, but we were all able to make it in one peace.
               I recommend visiting Lake Tahoe sometime. There are so many resorts and the slopes are long and wide.
-              This trip was truly one for the books. 
+              This trip was truly one for the books.
             </p>
           </Animator>
         </ScrollPage>
         <ScrollPage page={3}>
-          <Animator animation={batch(MoveIn(-1000,0), Sticky(20, 32), Fade())}>
-          <img
+          <Animator animation={batch(MoveIn(-1000, 0), Sticky(20, 32), Fade())}>
+            <img
               src={fatty_tuna_jp}
               alt={"Taken in Japan"}
-              style={{
-                width: 350,
-                height: 350,
-              }}
             />
           </Animator>
-          <Animator animation={batch(MoveIn(-1000,0), Sticky(42, 32), Fade())}>
-          <img
+          <Animator animation={batch(MoveIn(-1000, 0), Sticky(42, 32), Fade())}>
+            <img
               src={under_freeway_jp}
               alt={"Taken in Japan"}
-              style={{
-                width: 350,
-                height: 350,
-              }}
             />
           </Animator>
-          <Animator animation={batch(MoveIn(1000,0), Sticky(20,67), Fade())}>
-          <img
+          <Animator animation={batch(MoveIn(1000, 0), Sticky(20, 67), Fade())}>
+            <img
               src={sakura_jp}
               alt={"Taken in Japan"}
-              style={{
-                width: 350,
-                height: 350,
-              }}
             />
           </Animator>
-          <Animator animation={batch(MoveIn(1000,0), Sticky(42,67), Fade())}>
-          <img
+          <Animator animation={batch(MoveIn(1000, 0), Sticky(42, 67), Fade())}>
+            <img
               src={hakone_shrine_jp}
               alt={"Taken in Japan"}
-              style={{
-                width: 350,
-                height: 350,
-              }}
             />
           </Animator>
+
           <Animator animation={batch(Sticky(75), Fade())}>
             <p>
               You already know where this was taken judging by the pictures.
