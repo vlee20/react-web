b122c003b9bd41344ad8db5ed3aca4a0e78d5bf4
vlee20 <vlee20@protonmail.com>
2024-07-02 21:18:35 -0700

    group img

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,9 +9,12 @@ import { useEffect } from "react";
 
 import "../styles/Contact.css";
 
+
 import mammoth_mountain from "../img/R0000843.JPG";
 import sakura_jp from "../img/R0000961.JPG";
 import hakone_shrine_jp from "../img/R0001306.JPG";
+import fatty_tuna_jp from "../img/R0000918.JPG";
+import under_freeway_jp from "../img/R0000930.JPG";
 
 const boxVariant = {
   visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
@@ -43,7 +46,7 @@ const Box = ({ num }) => {
   );
 };
 
-const Group = ({ image }) => {
+const GroupImg = ({ image, desc }) => {
   const control = useAnimation();
   const [ref, inView] = useInView();
 
@@ -65,17 +68,18 @@ const Group = ({ image }) => {
     >
       <img
         src={image}
-        alt={"--"}
+        alt={"desc"}
         style={{
           maxWidth: 350,
           maxHeight: 350,
+          padding: 5
         }}
       />
     </motion.div>
   );
 };
 
-const ImageCont = ({ num }) => {
+const ImageCont = ({ image, desc }) => {
   const control = useAnimation();
   const [ref, inView] = useInView();
 
@@ -96,8 +100,8 @@ const ImageCont = ({ num }) => {
       animate={control}
     >
       <img
-        src={mammoth_mountain}
-        alt={"Taken leaving Mammoth Mountain"}
+        src={image}
+        alt={desc}
         style={{
           maxWidth: 700,
           maxHeight: 700,
@@ -122,10 +126,25 @@ export default function Contact() {
       <div className="App">
         <Box num={1} />
         <Box num={2} />
-        <ImageCont num={1} />
+        <ImageCont image={mammoth_mountain} desc={"Picture from Mammoth"} />
         <div className="container2">
-          <Group image={sakura_jp} />
-          <Group image={hakone_shrine_jp} />
+          <div className="container2">
+            <GroupImg image={sakura_jp} desc={"Picture from Japan"} />
+            <GroupImg image={hakone_shrine_jp} desc={"Picture from Japan"} />
+            <GroupImg image={fatty_tuna_jp} desc={"Picture from Japan"} />
+            <GroupImg image={under_freeway_jp} desc={"Picture from Japan"} />
+          </div>
+          <div>
+            <p style={{padding:30, alignContent: "center"}}>
+                You already know where this was taken judging by the pictures.
+                Can't explain how surreal this trip was because its JAPAN.
+                Theres really an endless amount of spots to explore around.
+                I've never seen rain so beautiful that it made me love it.
+                Also, I love listening to the sounds of the street lights and trains.
+                Matter of fact, their public transportation is far superior.
+                Miss every second truly....
+              </p>
+          </div>
         </div>
       </div>
     </>
