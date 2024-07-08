d2f02edff9ddfe14ada52fd4b302461c880f9fe2
vlee20 <lee.v3798@gmail.com>
2024-07-08 14:57:56 -0700

    center

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,10 +7,26 @@ import { useInView } from "react-intersection-observer";
 
 import { useEffect } from "react";
 
+import {
+  Animator,
+  ScrollContainer,
+  ScrollPage,
+  Sticky,
+  batch,
+  Fade,
+  Move,
+  MoveIn,
+  MoveOut,
+  FadeOut,
+  FadeIn,
+  ZoomIn,
+  StickyIn,
+} from "react-scroll-motion";
+
 import "../styles/Contact.css";
 
-
 import mammoth_mountain from "../img/R0000843.JPG";
+import lake_tahoe_lake from "../img/R0000886.JPG";
 import sakura_jp from "../img/R0000961.JPG";
 import hakone_shrine_jp from "../img/R0001306.JPG";
 import fatty_tuna_jp from "../img/R0000918.JPG";
@@ -53,8 +69,6 @@ const GroupImg = ({ image, desc }) => {
   useEffect(() => {
     if (inView) {
       control.start("visible");
-    } else {
-      control.start("hidden");
     }
   }, [control, inView]);
 
@@ -72,7 +86,7 @@ const GroupImg = ({ image, desc }) => {
         style={{
           maxWidth: 350,
           maxHeight: 350,
-          padding: 5
+          padding: 5,
         }}
       />
     </motion.div>
@@ -86,8 +100,6 @@ const ImageCont = ({ image, desc }) => {
   useEffect(() => {
     if (inView) {
       control.start("visible");
-    } else {
-      control.start("hidden");
     }
   }, [control, inView]);
 
@@ -104,7 +116,6 @@ const ImageCont = ({ image, desc }) => {
         alt={desc}
         style={{
           maxWidth: 700,
-          maxHeight: 700,
         }}
       />
     </motion.div>
@@ -116,18 +127,43 @@ export default function Contact() {
   useEffect(() => {
     if (inView) {
       control.start("visible");
-    } else {
-      control.start("hidden");
     }
   }, [control, inView]);
 
   return (
     <>
       <div className="App">
-        <Box num={1} />
-        <Box num={2} />
-        <ImageCont image={mammoth_mountain} desc={"Picture from Mammoth"} />
-        <div className="container2">
+        <div className="pgbreak">
+          <h2>Welcome to my Portfolio Page</h2>
+          <h3>This is an archive of the recent trips I've visited.</h3>
+          <h3>I like to travel if you can't tell!</h3>
+        </div>
+        <div className="container1">
+          <ImageCont image={mammoth_mountain} desc={"Picture from Mammoth"} />
+          <div>
+            <p>
+              This is located near the Mammoth Mountain resort. Mammoth has
+              always been an annual trip with friends. We always make memories
+              riding down the slopes. Snowboarding has been one of my favorite
+              sports because of how scenic and fun it is.
+            </p>
+          </div>
+        </div>
+        <div className="container1">
+          <ImageCont image={lake_tahoe_lake} desc={"Picture from Lake Tahoe"} />
+          <div>
+            <p>
+              This is located near the Palisades resort at Lake Tahoe. It was a
+              crazy drive from SoCal all the way to San Jose. Then from San Jose
+              to Lake Tahoe. The amount of time driving was insane because it
+              was during a storm, but we were all able to make it in one peace.
+              I recommend visiting Lake Tahoe sometime. There are so many
+              resorts and the slopes are long and wide. This trip was truly one
+              for the books.
+            </p>
+          </div>
+        </div>
+        <div className="container1">
           <div className="container2">
             <GroupImg image={sakura_jp} desc={"Picture from Japan"} />
             <GroupImg image={hakone_shrine_jp} desc={"Picture from Japan"} />
@@ -135,15 +171,15 @@ export default function Contact() {
             <GroupImg image={under_freeway_jp} desc={"Picture from Japan"} />
           </div>
           <div>
-            <p style={{padding:30, alignContent: "center"}}>
-                You already know where this was taken judging by the pictures.
-                Can't explain how surreal this trip was because its JAPAN.
-                Theres really an endless amount of spots to explore around.
-                I've never seen rain so beautiful that it made me love it.
-                Also, I love listening to the sounds of the street lights and trains.
-                Matter of fact, their public transportation is far superior.
-                Miss every second truly....
-              </p>
+            <p style={{ alignContent: "center" }}>
+              You already know where this was taken judging by the pictures.
+              Can't explain how surreal this trip was because its JAPAN. Theres
+              really an endless amount of spots to explore around. I've never
+              seen rain so beautiful that it made me love it. Also, I love
+              listening to the sounds of the street lights and trains. Matter of
+              fact, their public transportation is far superior. Miss every
+              second truly....
+            </p>
           </div>
         </div>
       </div>
